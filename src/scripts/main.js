e96a18e0c2fac9308071a550e4221ce45b2db864
Fredrik Broman <frebro@gmail.com>
2015-12-16 17:25:10 +0100

    Add basic interactions and animations on Pulsar and Tagline

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,3 +11,101 @@ ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
 WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
 ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE. */
+
+function ready(fn) {
+  if (document.readyState !== 'loading'){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+ready(function() {
+  let cloudCTALink = document.querySelectorAll('.CTA--cloud .CTA-link')[0];
+  let plmCTALink = document.querySelectorAll('.CTA--plm .CTA-link')[0];
+
+  cloudCTALink.addEventListener('mouseover', mouseEventHandler);
+  cloudCTALink.addEventListener('mouseout', mouseEventHandler);
+  cloudCTALink.addEventListener('click', mouseEventHandler);
+  plmCTALink.addEventListener('mouseover', mouseEventHandler);
+  plmCTALink.addEventListener('mouseout', mouseEventHandler);
+  plmCTALink.addEventListener('click', mouseEventHandler);
+});
+
+function mouseEventHandler(event) {
+  switch (event.type) {
+    case 'mouseover':
+      toggleBoxState('highlighted', event.currentTarget.dataset.slug);
+    break;
+    case 'mouseout':
+      toggleBoxState('highlighted', null);
+    break;
+    case 'click':
+      toggleBoxState('expanded', event.currentTarget.dataset.slug);
+    break;
+  }
+}
+
+function toggleBoxState(state, slug) {
+  let box = document.querySelectorAll('.Tagline')[0];
+
+  switch (state) {
+    case 'highlighted':
+      if (slug !== null) {
+        let className = 'is-'+slug+'Highlighted';
+        if (!hasClass(box, className)) {
+          addClass(box, className);
+        }
+      }
+      else {
+        if (hasClass(box, 'is-cloudHighlighted') && !hasClass(box, 'is-cloudExpanded')) {
+          removeClass(box, 'is-cloudHighlighted');
+        }
+        if (hasClass(box, 'is-plmHighlighted') && !hasClass(box, 'is-plmExpanded')) {
+          removeClass(box, 'is-plmHighlighted');
+        }
+      }
+    break;
+    case 'expanded':
+      if (slug !== null) {
+        let className = 'is-'+slug+'Expanded';
+        if (!hasClass(box, className)) {
+          box.style.minHeight = box.offsetHeight + 'px';
+          addClass(box, className);
+        }
+        else {
+          removeClass(box, className);
+        }
+      }
+      else {
+        if (hasClass(box, 'is-cloudExpanded')) {
+          removeClass(box, 'is-cloudExpanded');
+        }
+        if (hasClass(box, 'is-plmExpanded')) {
+          removeClass(box, 'is-plmExpanded');
+        }
+      }
+    break;
+  }
+}
+
+function hasClass(el, className) {
+  if (el.classList)
+    return el.classList.contains(className);
+  else
+    return new RegExp('(^| )' + className + '( |$)', 'gi').test(el.className);
+}
+
+function addClass(el, className) {
+  if (el.classList)
+    el.classList.add(className);
+  else
+    el.className += ' ' + className;
+}
+
+function removeClass(el, className) {
+  if (el.classList)
+    el.classList.remove(className);
+  else
+    el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+}
